ec3fc1c8b18be4093783cdda0384b70623b6e0ae
Carlos Lopez <carloslopez0517@gmail.com>
2020-05-12 01:00:34 -0400

    Fixed scapy IMPORT

--- Server.py
+++ Server.py
@@ -4,6 +4,7 @@ import socketserver
 import time
 from Conversions import *
 from scapy.sendrecv import send
+from scapy.all import *
 
 class ThreadedServerHandler (socketserver.BaseRequestHandler):
     def __init__(self, iterable):
@@ -101,6 +102,7 @@ class ClientInfo():
         if(passwoCrd == "PASSWORD"):
             self.elevated = True 
             self.send_message(construct_packet(self.address, "ACCESS GRANTED, WELCOME!"))
+            send(scapy_packet(self.address, "ACCESS GRANTED, WELCOME!"))
         else:
             self.elevated = False
 
@@ -115,37 +117,6 @@ class ClientInfo():
 
     def close(self):
         self.client.close()
-
-
-    # def send_cc_message(self, message):
-    #     send = "\u0030"
-    #     disconnect = "\u0031"
-    #     clients = "\u0032"
-    #     echo = "\u0033"
-    #     drop = "\u0034"
-    #     space = "\u0020"
-    #     message = send + '\u200c' + space + '\u200c' + "\u0079\u0065\u0065"
-    #     # message = message.encode()
-    #     if len(message) <= 1024:
-    #         packet = HTTP() / HTTPRequest(
-    #             Referer=message
-    #         )
-    #         self.send_message("RECEIVED")
-    #         self.send_message(packet[HTTPRequest].Referer)
-    #         # read_cc_message(packet)
-
-
-    # def read_cc_message(self, packet):
-    #     message2 = "message sent"
-    #     message = packet[HTTPRequest].Referer
-    #     if type(message) is not bytes:
-    #         message2 = message2.encode()
-    #         self.client.send(message2)
-    #         message = message.encode('utf-8', 'ignore')
-    #         self.client.send(message)
-    #     else:
-    #         self.client.send(message2)
-    #         self.client.send(message)
 
 
 class ThreadedServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
